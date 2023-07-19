f6b9e86a3cfc567389bda218e7ec906858baa92f
Mateus Caltabiano <mateus.frauzino@gmail.com>
2023-07-19 15:11:26 -0300

    add snackbar functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // TODO
 import FetchWrapper from "./fetch-wrapper.js";
+import { capitalize, calculateCalories } from "./helpers.js";
+import snackbar from "snackbar";
 
 const API = new FetchWrapper(
   "https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/6283"
@@ -28,12 +30,22 @@ form.addEventListener("submit", (event) => {
         "beforeend",
         `<li class="card">
           <div>
-            <h3 class="name">${foodName.value}</h3>
-            <div class="calories">0 calories</div>
+            <h3 class="name">${capitalize(foodName.value)}</h3>
+            <div class="calories">${calculateCalories(
+              carbs.value,
+              protein.value,
+              fat.value
+            )} calories</div>
             <ul class="macros">
-              <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
-              <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
-              <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+              <li class="carbs"><div>Carbs</div><div class="value">${
+                carbs.value
+              }g</div></li>
+              <li class="protein"><div>Protein</div><div class="value">${
+                protein.value
+              }g</div></li>
+              <li class="fat"><div>Fat</div><div class="value">${
+                fat.value
+              }g</div></li>
             </ul>
           </div>
          </li>`
@@ -43,6 +55,45 @@ form.addEventListener("submit", (event) => {
       carbs.value = "";
       protein.value = "";
       fat.value = "";
+
+      snackbar.show("Food added successfully.");
+    } else {
+      snackbar.show("Some data is missing.");
+      return;
     }
   });
 });
+
+const init = () => {
+  // TODO: Get the saved entries and list them
+  API.get("/").then((data) => {
+    data.documents?.forEach((document) => {
+      list.insertAdjacentHTML(
+        "beforeend",
+        `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(document.fields.name.stringValue)}</h3>
+          <div class="calories">${calculateCalories(
+            document.fields.carbs.integerValue,
+            document.fields.protein.integerValue,
+            document.fields.fat.integerValue
+          )} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${
+              document.fields.carbs.integerValue
+            }g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${
+              document.fields.protein.integerValue
+            }g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${
+              document.fields.fat.integerValue
+            }g</div></li>
+          </ul>
+        </div>
+      </li>`
+      );
+    });
+  });
+};
+
+init();
