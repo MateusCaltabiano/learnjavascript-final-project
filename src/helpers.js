55fb32e4d68479b6903d036416f20aa7fa19f94c
Mateus Caltabiano <mateus.frauzino@gmail.com>
2023-07-19 15:10:45 -0300

    add src/helpers.js to improve food log

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,6 @@
-export const capitalize = (word) => {
-  return word[0].toUpperCase() + word.substring(1).toLowerCase();
-};
+export const capitalize = (word) =>
+  (word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
 
-export const calculateCalories = (carbs = 0, protein = 0, fat = 0) => {
+export const calculateCalories = (carbs, protein, fat) => {
   return carbs * 4 + protein * 4 + fat * 9;
 };
